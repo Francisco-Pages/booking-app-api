74fcd64074602415e6f2ffa791915e88e03d86dd
Francisco-Pages <fj_pages@hotmail.com>
2023-05-25 16:32:28 -0400

    erase amenities

--- booking_app/rental_unit/serializers.py
+++ booking_app/rental_unit/serializers.py
@@ -3,12 +3,11 @@ serializers for rental unit API
 """
 from rest_framework import serializers
 
-from core.models import RentalUnit, Amenities
+from core.models import RentalUnit
 
 
 class RentalUnitSerializer(serializers.ModelSerializer):
     """serializer for rental unit"""
-    
     class Meta:
         model = RentalUnit
         fields = [
@@ -22,26 +21,14 @@ class RentalUnitSerializer(serializers.ModelSerializer):
             'unit_type', 
             'max_guests',
             'amenities',
-            'location', 
+            'location',
         ]
         read_only_fields = ['id']
         
+    
 class RentalUnitDetailSerializer(RentalUnitSerializer):
     """Serializer for rental unit detail view"""
     
     class Meta(RentalUnitSerializer.Meta):
         fields = RentalUnitSerializer.Meta.fields 
         
-class AmenitiesSerializer(serializers.ModelSerializer):
-    """serializer for an amenities list"""
-    
-    class Meta:
-        model = Amenities
-        fields = '__all__'
-        read_only_fields = ['id']
-        
-class AmenitiesDetailSerializer(AmenitiesSerializer):
-    """Serializer for amenities list detail view"""
-    
-    class Meta(AmenitiesSerializer.Meta):
-        fields = AmenitiesSerializer.Meta.fields 
--- booking_app/rental_unit/views.py
+++ booking_app/rental_unit/views.py
@@ -1,6 +1,8 @@
 """
 views for the rental unit api
 """
+from django.contrib.auth import get_user_model
+
 from rest_framework import viewsets
 from rest_framework import serializers as drf_serializers
 from rest_framework.authentication import TokenAuthentication
@@ -9,6 +11,13 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from core.models import RentalUnit, Amenities
 from rental_unit import serializers
 
+
+### HELPER FUNCTIONS ###
+def get_rental_units_of_user(user):
+    user = get_user_model().objects.get(user=user)
+    rental_units_of_user = RentalUnit.objects.filter(user=user)
+    
+    return [i for i in rental_units_of_user]
 
 class RentalUnitViewSet(viewsets.ModelViewSet):
     """view for manage rental unit APIs"""
@@ -19,7 +28,7 @@ class RentalUnitViewSet(viewsets.ModelViewSet):
     
     def get_queryset(self):
         """retrieve rental units for authenticated users"""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        return self.queryset.all().order_by('-id')
     
     def get_serializer_class(self):
         """returns serializer class for request"""
@@ -52,28 +61,28 @@ class RentalUnitViewSet(viewsets.ModelViewSet):
         else:
             instance.delete()
             
-class AmenitiesViewSet(viewsets.ModelViewSet):
-    """view for manage the amenities list for the rental unit APIs"""
-    serializer_class = serializers.AmenitiesDetailSerializer
-    queryset = Amenities.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+# class AmenitiesViewSet(viewsets.ModelViewSet):
+#     """view for manage the amenities list for the rental unit APIs"""
+#     serializer_class = serializers.AmenitiesDetailSerializer
+#     queryset = Amenities.objects.all()
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [IsAuthenticated]
     
-    def get_queryset(self):
-        """retrieve amenities list for authenticated users"""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+#     def get_queryset(self):
+#         """retrieve amenities list for authenticated users"""
+#         return self.queryset.all().order_by('-id')
     
-    def get_serializer_class(self):
-        """returns serializer class for request"""
-        if self.action == 'list':
-            return serializers.AmenitiesSerializer
-        return self.serializer_class
+#     def get_serializer_class(self):
+#         """returns serializer class for request"""
+#         if self.action == 'list':
+#             return serializers.AmenitiesSerializer
+#         return self.serializer_class
     
-    def perform_create(self, serializer):
-        """create a new amenities list"""
-        user = self.request.user
-        
-        if user.is_staff == False:
-            raise drf_serializers.ValidationError('Not authorized to create a list of amenities')
-        else:
-            serializer.save(user=user)
+    # def perform_create(self, serializer):
+    #     """create a new amenities list"""
+    #     user = self.request.user
+    #     rental_unit_of_user = RentalUnit.objects.filter(user=user)
+    #     if user.is_staff == False:
+    #         raise drf_serializers.ValidationError('Not authorized to create a list of amenities')
+    #     if rental_unit_of_user.rental_unit_id not in RentalUnit.objects.filter:
+    #         serializer.save(user=user)
