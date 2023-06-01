f0f834d4094a02a7c43f1305c4ac0caec4cc7e8b
Francisco-Pages <fj_pages@hotmail.com>
2023-06-01 15:03:33 -0400

    write tests for calendar event api

---
+++ booking_app/rental_unit/tests/test_calendar_event_api.py
@@ -0,0 +1,315 @@
+"""
+tests for calendar_event API
+"""
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse 
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import RentalUnit, CalendarEvent
+
+from rental_unit.serializers import (
+    CalendarEventSerializer,
+    CalendarEventDetailSerializer
+)
+
+
+CALENDAR_EVENT_URL = reverse('rental_unit:calendarevent-list')
+
+def detail_url(calendar_event_id):
+    """create and return a detailed calendar_event URL"""
+    return reverse('rental_unit:calendarevent-detail', args=[calendar_event_id])
+
+def create_rental_unit(user, **params):
+    """create and return a rental unit object"""
+    defaults = {
+        'title':'Title of property',
+        'description':'A unique description of your home',
+        'unit_type':'Apartment',
+        'status':'Inactive',
+        'max_guests':1,
+    }
+    defaults.update(params)
+
+    rental_unit = RentalUnit.objects.create(user=user, **defaults)
+    return rental_unit
+
+# def create_calendar_event(rental_unit_id, **params):
+#     """create and return a calendar_event object"""
+#     defaults = {
+#         'name':'Pet',
+#         'price':Decimal('25.50'),
+#     }
+#     defaults.update(params)
+    
+#     return CalendarEvent.objects.create(rental_unit=rental_unit_id, **defaults)
+    
+
+def create_user(**params):
+    """create and return a new user"""
+    return get_user_model().objects.create_user(**params)
+
+def create_superuser(**params):
+    """create and return a new user"""
+    return get_user_model().objects.create_superuser(**params)
+
+### TEST HANDLERS ###
+class PublicCalendarEventApiTests(TestCase):
+    """tests for unauthenticated API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_calendar_event_by_non_auth(self):
+        """test that unauthenticated requests can read CalendarEvents"""
+        result = self.client.get(CALENDAR_EVENT_URL)
+
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        
+    def test_get_calendar_event_detail_by_non_auth(self):
+        """test that an unauthenticated request can read a detailed calendar_event"""
+        user = create_user(
+            email='test@example.com',
+            password='testpass123'
+        )
+        rental_unit = create_rental_unit(user=user)
+        calendar_event = CalendarEvent.objects.create(rental_unit=rental_unit, reason='Reserved')
+        
+        url = detail_url(calendar_event.id)
+        result = self.client.get(url)
+        serializer = CalendarEventDetailSerializer(calendar_event)
+
+        self.assertEqual(result.data, serializer.data)
+        
+
+class PrivateCalendarEventApiTests(TestCase):
+    """test for authenticated API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_user(
+            email='test@example.com', 
+            password='test1234'
+        )
+        self.client.force_authenticate(user=self.user)
+        
+    def test_get_all_calendar_events_list(self):
+        """test retrieving a list of CalendarEvents"""
+        rental_unit = create_rental_unit(user=self.user)
+        rental_unit_two = create_rental_unit(user=self.user)
+        
+        CalendarEvent.objects.create(rental_unit=rental_unit, reason='Reserved')
+        CalendarEvent.objects.create(rental_unit=rental_unit_two, reason='Reserved')
+        
+        result = self.client.get(CALENDAR_EVENT_URL)
+        
+        calendar_events = CalendarEvent.objects.all().order_by('-rental_unit')
+        serializer = CalendarEventSerializer(calendar_events, many=True)
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertEqual(result.data, serializer.data)
+        
+    def test_get_calendar_event_detail(self):
+        """test get rental unit detail"""
+        rental_unit = create_rental_unit(user=self.user)
+        calendar_event = CalendarEvent.objects.create(rental_unit=rental_unit, reason='Reserved')
+        
+        url = detail_url(calendar_event.id)
+        result = self.client.get(url)
+
+        serializer = CalendarEventDetailSerializer(calendar_event)
+        self.assertEqual(result.data, serializer.data)
+        
+    # def test_error_create_calendar_event(self):
+    #     """test error creating a CalendarEvent by a non administrator"""
+    #     rental_unit = create_rental_unit(user=self.user)
+        
+    #     payload = {
+    #         'rental_unit': rental_unit.id,
+    #         'reason': 'Reserved'
+    #     }
+    #     result = self.client.post(CALENDAR_EVENT_URL, payload)
+
+    #     self.assertEqual(result.status_code, status.HTTP_403_FORBIDDEN)
+    #     self.assertFalse(CalendarEvent.objects.filter(rental_unit=payload['rental_unit']).exists())
+        
+    # def test_error_partial_update(self):
+    #     """test error patch of a calendar_event by a non administrator"""
+    #     rental_unit = create_rental_unit(user=self.user)
+    #     original_price = Decimal('20')
+    #     original_description = 'small pets only.'
+        
+    #     calendar_event = CalendarEvent.objects.create(
+    #         rental_unit=rental_unit,
+    #         price=original_price,
+    #         description=original_description, 
+    #     )
+
+    #     payload = {
+    #         'rental_unit': calendar_event.rental_unit.id,
+    #         'price': Decimal('30'),
+    #     }
+        
+    #     url = detail_url(calendar_event.id)
+    #     result = self.client.patch(url, payload)
+        
+    #     self.assertEqual(result.status_code, status.HTTP_403_FORBIDDEN)
+    #     calendar_event.refresh_from_db()
+    #     self.assertEqual(calendar_event.price, original_price)
+        
+    # def test_error_full_update(self):
+    #     """test error put of calendar_event by non admin"""
+    #     rental_unit = create_rental_unit(user=self.user)
+    #     original_price = Decimal('20')
+    #     original_description = 'small pets only.'
+    #     original_name = 'Pet'
+    
+    #     calendar_event = CalendarEvent.objects.create(
+    #         rental_unit=rental_unit,
+    #         price=original_price,
+    #         description=original_description,
+    #         name=original_name,
+    #     )
+        
+    #     payload = {
+    #         'rental_unit':rental_unit.id,
+    #         'price': Decimal('30'),
+    #         'name': 'Transport',
+    #         'description': 'pickup and dropoff'
+    #     }
+    #     url = detail_url(calendar_event.id)
+        
+    #     result = self.client.put(url, payload)
+
+    #     self.assertEqual(result.status_code, status.HTTP_403_FORBIDDEN)
+    #     calendar_event.refresh_from_db()
+        
+    #     self.assertEqual(calendar_event.price, original_price)
+        
+    def test_error_delete_calendar_event(self):
+        """test error when deleting a CalendarEvent by a non admin"""
+        rental_unit = create_rental_unit(user=self.user)
+        calendar_event = CalendarEvent.objects.create(rental_unit=rental_unit, reason='Reserved')
+        
+        url = detail_url(calendar_event.id)
+        result = self.client.delete(url)
+        
+        self.assertEqual(result.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue(CalendarEvent.objects.filter(id=calendar_event.id).exists())
+        
+        
+class AdminCalendarEventApiTests(TestCase):
+    """test authorized API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_superuser(
+            email='testadmin@example.com',
+            password='testpass123'
+        )
+        self.client.force_authenticate(user=self.user)
+    
+    # def test_create_calendar_event(self):
+    #     """test creating a CalendarEvent by an administrator"""
+    #     rental_unit = create_rental_unit(user=self.user)
+        
+    #     payload = {
+    #         'rental_unit': rental_unit.id,
+    #         'name': 'Transport'
+    #     }
+    #     result = self.client.post(CALENDAR_EVENT_URL, payload)
+
+    #     self.assertTrue(CalendarEvent.objects.filter(rental_unit=payload['rental_unit'], name=payload['name']).exists())
+    #     self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        
+    # def test_error_create_double_calendar_event(self):
+    #     """test that there can only be one type of calendar_event for one rental unit"""
+    #     rental_unit = create_rental_unit(user=self.user)
+    #     calendar_event = CalendarEvent.objects.create(
+    #         rental_unit=rental_unit,
+    #         name='Pet',
+    #         price=Decimal('50.51')
+    #     )
+        
+    #     payload = {
+    #         'rental_unit': rental_unit.id,
+    #         'name': 'Pet',
+    #         'price': Decimal('50.50')
+    #     }
+    #     result = self.client.post(CALENDAR_EVENT_URL, payload)
+        
+    #     self.assertEqual(calendar_event.price, Decimal('50.51'))
+    #     self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+    #     self.assertFalse(CalendarEvent.objects.filter(rental_unit=payload['rental_unit'], name=payload['name'], price=payload['price']).exists())
+        
+        
+    # def test_partial_update(self):
+    #     """test patch of a calendar_event by an administrator"""
+    #     rental_unit = create_rental_unit(user=self.user)
+    #     original_price = Decimal('20')
+    #     original_description = 'small pets only.'
+        
+    #     calendar_event = CalendarEvent.objects.create(
+    #         rental_unit=rental_unit,
+    #         price=original_price,
+    #         description=original_description, 
+    #     )
+
+    #     payload = {
+    #         'rental_unit': calendar_event.rental_unit.id,
+    #         'price': Decimal('30'),
+    #     }
+        
+    #     url = detail_url(calendar_event.id)
+    #     result = self.client.patch(url, payload)
+        
+    #     self.assertEqual(result.status_code, status.HTTP_200_OK)
+    #     calendar_event.refresh_from_db()
+    #     self.assertEqual(calendar_event.price, payload['price'])
+    #     self.assertEqual(calendar_event.description, original_description)
+        
+    # def test_full_update(self):
+    #     """test put of calendar_event"""
+    #     rental_unit = create_rental_unit(user=self.user)
+    #     original_price = Decimal('20')
+    #     original_description = 'small pets only.'
+    #     original_name = 'Pet'
+    
+    #     calendar_event = CalendarEvent.objects.create(
+    #         rental_unit=rental_unit,
+    #         price=original_price,
+    #         description=original_description,
+    #         name=original_name,
+    #     )
+        
+    #     payload = {
+    #         'rental_unit':rental_unit.id,
+    #         'price': Decimal('30'),
+    #         'name': 'Transport',
+    #         'description': 'pickup and dropoff'
+    #     }
+    #     url = detail_url(calendar_event.id)
+        
+    #     result = self.client.put(url, payload)
+
+    #     self.assertEqual(result.status_code, status.HTTP_200_OK)
+    #     calendar_event.refresh_from_db()
+        
+    #     for k, v in payload.items():
+    #         if k == 'rental_unit':
+    #             continue
+    #         self.assertEqual(getattr(calendar_event, k), v)
+        
+    def test_delete_calendar_event(self):
+        """test deleting a CalendarEvent is successful"""
+        rental_unit = create_rental_unit(user=self.user)
+        calendar_event = CalendarEvent.objects.create(rental_unit=rental_unit, reason='Reserved')
+        
+        url = detail_url(calendar_event.id)
+        result = self.client.delete(url)
+        
+        self.assertEqual(result.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(CalendarEvent.objects.filter(id=calendar_event.id).exists())
